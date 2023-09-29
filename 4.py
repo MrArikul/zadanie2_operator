542b112570b7253490d44c807d34c4704964bf9b
Oleg Pereverzev <MrArikul@mail.ru>
2023-09-29 20:26:51 +0700

    zadanie2_operator

---
+++ 4.py
@@ -0,0 +1,11 @@
+x = int(input("Число x: "))
+y = int(input("Число y: "))
+
+if x > 0 and y > 0:
+	print("1 четверть")
+elif x < 0 and y > 0:
+	print("2 четверть")
+elif x < 0 and y < 0:
+	print("3 четверть")
+elif x > 0 and y < 0:
+	print("4 четверть")
